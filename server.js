d3b775fb0a8198ccb2abe28c9de7a4ca812e4e58
kamal-gaan <kamalpandi.v@gaansoft.com>
2025-03-26 14:40:04 +0530

    node js snap 3

--- server.js
+++ server.js
@@ -16,30 +16,33 @@ const server = http.createServer(async (req, res) => {
         if (req.method === 'GET') {
             let filepath;
             if (req.url === '/') {
+
                 filepath = path.join(__dirname, 'public', 'index.html');
-                // res.setHeader('Content-Type', 'text/html');
-                // res.statusCode = 200;
-                // res.end(html_content);
+
             } else if (req.url === '/about') {
+
                 filepath = path.join(__dirname, 'public', 'about.html');
-                // res.writeHead(200, { 'Content-type': 'text/html' });
-                // res.end('<h1>About page</h1>');
+
             } else {
+
                 throw new Error('Not found');
-                // res.writeHead(404, { 'Content-type': 'text/html' });
-                // res.end('<h1>Page not found</h1>');
+
             }
+
             const data = await fs.readFile(filepath);
             res.setHeader('Content-type', 'text/html');
             res.write(data);
             res.end();
+        
         } else {
             throw new Error('Method not allowd');
         }
 
     } catch (error) {
+        
         res.writeHead(500, { 'Content-type': 'text/plain' });
         res.end('Server error:', error);
+    
     }
 
 
