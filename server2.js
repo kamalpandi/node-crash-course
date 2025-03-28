1d1737e220202f285fceaef3d04c0b7691fa58bf
kamal-gaan <kamalpandi.v@gaansoft.com>
2025-03-28 14:20:48 +0530

    node js snap 6

--- server2.js
+++ server2.js
@@ -3,9 +3,9 @@ import { createServer } from 'http';
 const PORT = process.env.PORT;
 
 const users = [
-    { id: 1, name: 'john doe' },
-    { id: 2, name: 'jane doe' },
-    { id: 3, name: 'jim doe' }
+    { id: 0, name: 'john doe' },
+    { id: 1, name: 'jane doe' },
+    { id: 2, name: 'jim doe' }
 ];
 
 const server = createServer((req, res) => {
@@ -13,6 +13,22 @@ const server = createServer((req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify(users));
         res.end();
+        
+    } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+        const id = req.url.split('/')[3]; // third slice of the url
+        console.log(id)
+        const user = users.find((user) => user.id === parseInt(id));
+        res.setHeader('Content-Type', 'application/json');
+        // res.write(JSON.stringify(users[parseInt(id)]));  
+
+        if (user) {
+            res.write(JSON.stringify(user));
+        } else {
+            res.statusCode = 404;
+            res.write(JSON.stringify({ message: `User ${id} not found` }));
+        }
+        res.end();
+
     } else {
         res.setHeader('Content-Type', 'application/json');
         res.statusCode = 404;
